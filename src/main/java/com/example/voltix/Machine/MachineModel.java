d6c282775bb168b99d8f6aa9df8d29660a3b35af
aziz gadacha <aziz.gadacha@esprit.tn>
2023-07-31 11:26:52 +0100

    adding model

---
+++ src/main/java/com/example/voltix/Machine/MachineModel.java
@@ -0,0 +1,13 @@
+package com.example.voltix.Machine;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
+
+@Data
+@Document(collection = "Machines")
+public class MachineModel {
+    @Id
+    private String id;
+    private String name;
+
+}
